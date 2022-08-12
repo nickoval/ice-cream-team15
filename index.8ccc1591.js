6893f3d32890283429d74649a090a0b33cee9205
nickoval <85960879+nickoval@users.noreply.github.com>
2022-08-12 18:00:12 +0000

    Deploying to gh-pages from @ nickoval/ice-cream-team15@4f2ea8c75a885e9bb246f286086e1c7f6b064943 🚀

--- index.8ccc1591.js
+++ index.8ccc1591.js
@@ -1,2 +1,2 @@
 !function(){var e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.a081b212.js.map
+//# sourceMappingURL=index.8ccc1591.js.map
---
+++ index.e8045a66.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.e8045a66.js.map

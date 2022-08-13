36e362d27cce5b86966f086b2ab811531b31ff64
nickoval <85960879+nickoval@users.noreply.github.com>
2022-08-13 09:03:37 +0000

    Deploying to gh-pages from @ nickoval/ice-cream-team15@a4f467735ef78b9025eb7ef55c2d6238f179f5b5 🚀

---
+++ index.0fba3b5e.js
@@ -0,0 +1,2 @@
+(()=>{const t=document.querySelector("[data-description-button]"),e=document.querySelector("[data-description]");t.addEventListener("click",(()=>{const i="true"===t.getAttribute("aria-expanded")||!1;t.classList.toggle("is-open"),t.setAttribute("aria-expanded",!i),e.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.0fba3b5e.js.map
---
+++ index.400abd00.js
@@ -0,0 +1,2 @@
+!function(){var t,e;t=document.querySelector("[data-description-button]"),e=document.querySelector("[data-description]"),t.addEventListener("click",(function(){var i="true"===t.getAttribute("aria-expanded")||!1;t.classList.toggle("is-open"),t.setAttribute("aria-expanded",!i),e.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.400abd00.js.map

779974abaa4a7efc5d01b3a6e3ddfa2aeb88abc5
nickoval <85960879+nickoval@users.noreply.github.com>
2022-08-13 21:07:51 +0000

    Deploying to gh-pages from @ nickoval/ice-cream-team15@b423a8171ea8529d51f8201061ebad61836e698b 🚀

---
+++ index.7b21caf2.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-for-contacts-open]"),closeModalBtn:document.querySelector("[data-modal-for-contacts-close]"),modal:document.querySelector("[data-modal-for-contacts]")};function t(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",t),o.closeModalBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.7b21caf2.js.map

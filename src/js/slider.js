7d53f80fcc98a6a8c18703ac972972179b41a3e7
Darmoroz <darmoroz99@gmail.com>
2022-08-14 11:41:44 +0300

    mobile add

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 	$('.slider').slick({
-		arrows:true,
+		arrows:false,
 		dots:true,
 		// adaptiveHeight:false, 2
 		slidesToShow:1,
 		slidesToScroll:1,
 		speed:1000,
 		// easing:'cubic-bezier(0,0,1,0)',
-		infinite:true,
-		initialSlide:3,
-		autoplay:true,
-		autoplaySpeed:3000,
+		// infinite:true,
+		// initialSlide:3,
+		// autoplay:true,
+		// autoplaySpeed:3000,
 		// pauseOnFocus:true, 3
 		// pauseOnHover:true, 3
 		// pauseOnDotsHover:true, 3
@@ -21,8 +21,8 @@ $(document).ready(function() {
 		waitForAnimate:true,
 		centerMode:false, // вкл головни слайд завжди по центру
 		variableWidth:false,
-		rows:1,
-		slidesPerRow:1,
+		// rows:1,
+		// slidesPerRow:1,
 		// vertical:true, 4
 		// verticalSwiping:true, 4
 	});

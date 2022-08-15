24066c57a4affc7826a0e55716c963fd5ae0a3c0
nickoval <mykovalmail@gmail.com>
2022-08-16 01:03:25 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -22,7 +22,6 @@
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");

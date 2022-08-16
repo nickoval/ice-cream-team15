aa09aa1b781ac41bac02fdcea5204337befcdec0
nickoval <85960879+nickoval@users.noreply.github.com>
2022-08-16 06:00:27 +0000

    Deploying to gh-pages from @ nickoval/ice-cream-team15@806a1749e26ae400f46e3098543320cda179a718 🚀

---
+++ index.672e426f.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-about-modal-open]"),closeModalBtn:document.querySelector("[data-about-modal-close]"),modal:document.querySelector("[data-about-modal]")};function e(){o.modal.classList.toggle("backdrop-about__is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.672e426f.js.map
---
+++ index.c59657c4.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-about-modal-open]"),closeModalBtn:document.querySelector("[data-about-modal-close]"),modal:document.querySelector("[data-about-modal]")};function e(){o.modal.classList.toggle("backdrop-about__is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.c59657c4.js.map
